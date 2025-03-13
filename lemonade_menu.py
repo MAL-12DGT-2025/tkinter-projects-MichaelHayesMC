68815a66e52f5d669708f374fb75c264ce64ac54
MichaelHayesMC <michael.hayes@marcellin.school.nz>
2025-03-14 11:01:12 +1300

    Added Price Change

--- lemonade_menu.py
+++ lemonade_menu.py
@@ -6,14 +6,15 @@ icecube_price = 0.50
 lemonade_price = 2.00
 
 def price_calc(i):
-    amount = float(count.get())
-    print(float(amount))
-    price1.config(text=f"${amount*lemon_price: .2f}")
+    #print(i)
+    amount = (count.get())
+    price1.config(text=f"${amount*lemon_price:.2f}")
+    purchase_L.config(text=f"${amount*lemon_price:.2f}")
 
 root = tk.Tk()
 root.title("Lemonade Stand Menu")
 
-count = tk.IntVar()
+count = tk.IntVar(value=1)
 
 title = ttk.Label(root, text="Lemonade Stand Menu")
 title.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
@@ -39,13 +40,13 @@ selected_2.grid(row=3, column=0)
 selected_3 = ttk.Checkbutton(root, onvalue=1, offvalue=0)
 selected_3.grid(row=4, column=0)
 
-price1 = ttk.Label(root, text="$0.00")
+price1 = ttk.Label(root, text=f"${lemon_price:.2f}")
 price1.grid(row=2, column=3, padx=10)
 
-price2 = ttk.Label(root, text="$0.00")
+price2 = ttk.Label(root, text=f"${icecube_price:.2f}")
 price2.grid(row=3, column=3, padx=10)
 
-price3 = ttk.Label(root, text="$0.00")
+price3 = ttk.Label(root, text=f"${lemonade_price:.2f}")
 price3.grid(row=4, column=3, padx=10)
 
 item1 = ttk.Label(root, text="50g Lemons")
@@ -61,10 +62,10 @@ quantity1 = ttk.Entry(root, width=5, textvariable=count)
 quantity1.grid(row=2, column=1, padx=5)
 quantity1.bind("<Return>", price_calc)
 
-quantity2 = ttk.Entry(root, width=5)
+quantity2 = ttk.Entry(root, width=5, textvariable=count)
 quantity2.grid(row=3, column=1, padx=5)
 
-quantity3 = ttk.Entry(root, width=5)
+quantity3 = ttk.Entry(root, width=5, textvariable=count)
 quantity3.grid(row=4, column=1, padx=5)
 
 purchase_L = ttk.Button(root, text="(Cost)")

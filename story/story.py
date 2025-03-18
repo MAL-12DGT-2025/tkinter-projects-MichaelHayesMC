f7867c814fb287914e1be2cbd6c0bd5f80245340
MichaelHayesMC <michael.hayes@marcellin.school.nz>
2025-03-18 15:13:04 +1300

    Change

---
+++ story/story.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+from tkinter import ttk
+
+def button_push():
+    bible_verse = input_entry.get()
+    print(bible_verse)
+    bible_verse_label.config(text=bible_verse)
+    with open("bible_verse.txt", "w") as file:
+        file.write(bible_verse)
+
+
+root = tk.Tk()
+root.title("Bible Verse Text Reader")
+
+title = ttk.Label(root, text="Bible Verse Text Reader")
+title.grid(row=0, pady=10)
+
+prompt_label = ttk.Label(root, text="Please input your Bible Verse:")
+prompt_label.grid(row=1, padx=20)
+
+input_entry = ttk.Entry(root)
+input_entry.grid(row=2)
+
+process = ttk.Button(root, text="Process", command=button_push)
+process.grid(row=3)
+
+bible_verse_label = ttk.Label(root, text="Bible Verse:")
+bible_verse_label.grid(row=4,pady=20)
+
+root.mainloop()
